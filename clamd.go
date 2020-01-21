ef68408c3534f809cae1b50aedba92fa0702c593
Christian Pointner <cpointner@mgit.at>
2020-01-21 14:32:57 +0100

    clamd stats metrics (WIP)

--- clamd.go
+++ clamd.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"errors"
+	"log"
 	"regexp"
 	"strconv"
 	"time"
@@ -18,7 +19,10 @@ const (
 )
 
 var (
-	clamdVersionRegexp = regexp.MustCompile(`ClamAV (?P<clamav_version>.*?)/(?P<db_version>.*?)/(?P<db_date>.*?)$`)
+	clamdVersionRegexp = regexp.MustCompile(`^ClamAV (?P<clamav_version>.*?)/(?P<db_version>.*?)/(?P<db_date>.*?)$`)
+
+	clamdStatsQueueRegexp   = regexp.MustCompile(`^(\d+)\s+item.*$`)
+	clamdStatsThreadsRegexp = regexp.MustCompile(`^live\s+(\d+)$`)
 )
 
 type ClamDOptions struct {
@@ -28,9 +32,10 @@ type ClamDOptions struct {
 type ClamDChecker struct {
 	opts ClamDOptions
 
-	promClamDUp        *prometheus.Desc
-	promClamDDBVersion *prometheus.Desc
-	promClamDDBTime    *prometheus.Desc
+	promClamDUp               *prometheus.Desc
+	promClamDDBVersion        *prometheus.Desc
+	promClamDDBTime           *prometheus.Desc
+	promClamDStatsQueueLength *prometheus.Desc
 }
 
 func NewClamDChecker(opts ClamDOptions) *ClamDChecker {
@@ -51,6 +56,11 @@ func NewClamDChecker(opts ClamDOptions) *ClamDChecker {
 			"timestamp of currently used virus definition DB",
 			[]string{},
 			nil),
+		promClamDStatsQueueLength: prometheus.NewDesc(
+			"clamav_clamd_stats_queue_length",
+			"clamd queue length",
+			[]string{},
+			nil),
 	}
 }
 
@@ -58,6 +68,7 @@ func (c *ClamDChecker) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.promClamDUp
 	ch <- c.promClamDDBVersion
 	ch <- c.promClamDDBTime
+	ch <- c.promClamDStatsQueueLength
 }
 
 func (c *ClamDChecker) Collect(ch chan<- prometheus.Metric) {
@@ -82,6 +93,13 @@ func (c *ClamDChecker) Collect(ch chan<- prometheus.Metric) {
 		c.promClamDDBTime,
 		prometheus.GaugeValue,
 		float64(dbTime.Unix()),
+	)
+
+	stats, _ := c.collectStats()
+	ch <- prometheus.MustNewConstMetric(
+		c.promClamDStatsQueueLength,
+		prometheus.GaugeValue,
+		float64(stats.QueueLength),
 	)
 }
 
@@ -113,3 +131,128 @@ func (c *ClamDChecker) collectVersion() (version string, dbVersion int, dbTime t
 
 	return
 }
+
+type clamdStats struct {
+	QueueLength int
+	Threads     struct {
+		Live        int
+		Idle        int
+		Max         int
+		IdleTimeout int
+	}
+}
+
+func (c *ClamDChecker) collectStats() (stats clamdStats, err error) {
+	stats.QueueLength = -1
+	stats.Threads.Live = -1
+	stats.Threads.Idle = -1
+	stats.Threads.Max = -1
+	stats.Threads.IdleTimeout = -1
+
+	var cl *clamd.ClamD
+	if cl, err = clamd.NewClamd(c.opts.URL); err != nil {
+		return
+	}
+
+	var s *clamd.ClamDStats
+	if s, err = cl.Stats(); err != nil {
+		return
+	}
+	log.Printf("%#v", s)
+
+	q := clamdStatsQueueRegexp.FindStringSubmatch(s.Queue)
+	if len(q) == 2 {
+		stats.QueueLength, _ = strconv.Atoi(q[1])
+	}
+
+	return
+
+	// 	statsData := clamdStats{}
+	// 	if inum,err := strconv.Atoi(stats.Pools); err == nil {
+	// 		statsData.Pools = inum
+	// 	}
+	// 	statsData.State = strings.Replace(stats.State,"STATE: ","",-1)
+	// 	if inum,err := strconv.Atoi(getNumber.FindString(stats.Queue)); err == nil {
+	// 		statsData.Queue = inum
+	// 	}
+
+	// 	threads := getNumber.FindAllString(stats.Threads,-1)
+	// 	if len(threads) == 4 {
+	// 		if inum,err := strconv.ParseFloat(threads[0], 32); err == nil {
+	// 			statsData.Threads.Live = inum
+	// 		}
+	// 		if inum,err := strconv.ParseFloat(threads[1], 32); err == nil {
+	// 			statsData.Threads.Idle = inum
+	// 		}
+
+	// 		if inum,err := strconv.ParseFloat(threads[2], 32); err == nil {
+	// 			statsData.Threads.Max = inum
+	// 		}
+	// 		if inum,err := strconv.ParseFloat(threads[3], 32); err == nil {
+	// 			statsData.Threads.Timeout = inum
+	// 		}
+	// 	}
+	// 	mem := getNumber.FindAllString(stats.Memstats,-1)
+	// 	memLen := len(mem)
+	// 	if memLen >= 4 {
+	// 		if inum, err := strconv.ParseFloat(mem[0], 32 ); err == nil {
+	// 			statsData.Memory.Heap = inum * 1024.0 * 1024.0
+	// 		}
+	// 		if inum, err := strconv.ParseFloat(mem[1], 32 ); err == nil {
+	// 			statsData.Memory.Mmap = inum * 1024 * 1024
+	// 		}
+	// 		if inum, err := strconv.ParseFloat(mem[2], 32); err == nil {
+	// 			statsData.Memory.Used = inum * 1024 * 1024
+	// 		}
+	// 		if inum, err := strconv.ParseFloat(mem[3], 32); err == nil {
+	// 			statsData.Memory.Free = inum * 1024 * 1024
+	// 		}
+	// 		poolcount, _ := strconv.Atoi(mem[5])
+	// 		pools := make([]clamdStatsMemPool,poolcount)
+	// 		for i := 0; i < poolcount; i++ {
+	// 			idx := 6 + i * 2
+	// 			if inum,err := strconv.ParseFloat(mem[idx], 32 ); err == nil {
+	// 				pools[i].Used = inum * 1024 * 1024
+	// 			}
+	// 			if inum,err := strconv.ParseFloat( mem[idx + 1], 32); err == nil {
+	// 				pools[i].Total = inum * 1024 * 1024
+	// 			}
+	// 		}
+	// 		statsData.Memory.Pools = pools
+	// 	}
+	// 	clamd_stats.Stats = statsData
+
+	// 	return clamd_stats
+
+}
+
+// func getclamdStats(url string) (*clamdData) {
+
+// 	getNumber := regexp.MustCompile("[.0-9]+")
+
+// 	c := clamd.NewClamd(url)
+// 	clamd_stats := &clamdData{}
+// 	clamd_stats.Err = ""
+
+// 	start := time.Now()
+// 	err := c.Ping()
+// 	elapsed := time.Since(start)
+// 	if err != nil {
+// 		clamd_stats.Err = err.Error()
+// 		return clamd_stats
+// 	}
+// 	clamd_stats.Ping = elapsed
+
+// 	reader := bytes.NewReader(clamd.EICAR)
+// 	chanFoo := make(chan bool)
+
+// 	start = time.Now()
+// 	_, err = c.ScanStream(reader, chanFoo)
+// 	elapsed = time.Since(start)
+// 	if err != nil {
+// 		clamd_stats.Err = err.Error()
+// 		return clamd_stats
+// 	}
+// 	clamd_stats.EicarScan = elapsed
+
+// }
